325c5658bc30df454693b435ad6740477aaaac53
Ken Shimizu <ashlynn.ny.2004@gmail.com>
2022-12-30 00:14:04 +0900

    add tensorflow

--- mycam/MyArducam.cpp
+++ mycam/MyArducam.cpp
@@ -20,6 +20,16 @@
 #include "XBee.h"
 #include "XBeePico.h"
 
+//#include "detection_responder.h"
+//#include "image_provider.h"
+#include "model_settings.h"
+#include "person_detect_model_data.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
 #ifndef max
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #define min(a,b) (((a) < (b)) ? (a) : (b))
@@ -41,6 +51,18 @@
 #define CMD_ARDUCAM_CMD 0x17
 
 #define DATA_SIZE 80
+
+namespace {
+    tflite::ErrorReporter    *error_reporter = nullptr;
+    const tflite::Model      *model          = nullptr;
+    tflite::MicroInterpreter *interpreter    = nullptr;
+    TfLiteTensor             *input          = nullptr;
+
+    constexpr int  kTensorArenaSize = 54 * 1024 + 27 * 1024;
+    static uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
+
+
 
 enum send_stat {
   ST_INIT,
@@ -401,6 +423,40 @@ int main()
 
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
+
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  model = tflite::GetModel(g_person_detect_model_data);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return -1;
+  }
+
+  static tflite::MicroMutableOpResolver<5> micro_op_resolver;
+  micro_op_resolver.AddAveragePool2D();
+  micro_op_resolver.AddConv2D();
+  micro_op_resolver.AddDepthwiseConv2D();
+  micro_op_resolver.AddReshape();
+  micro_op_resolver.AddSoftmax();
+
+  static tflite::MicroInterpreter static_interpreter(
+    model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  // Allocate memory from the tensor_arena for the model's tensors.
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return -2;
+  }
+
+  // Get information about the memory area to use for the model's input.
+  input = interpreter->input(0);
+
 
   gpio_put(LED_PIN, 1);
 
